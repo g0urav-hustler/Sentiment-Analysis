72513a5fa6afd62de0191b8cfa930a5024550ca4
g0urav_hustler <gouravchouhan966@gmail.com>
2021-12-18 17:12:39 +0530

    updating html files

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask , render_template, request
 import tensorflow as tf
-from tensorflow.keras.datasets import imdb
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.preprocessing.text import one_hot
+import nltk
+nltk.download(['stopwords', 'wordnet'])
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 import pandas as pd
 import numpy as np
 import re
@@ -14,34 +18,40 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 
-model = load_model('sentiment_analysis.h5')
+model = load_model('models/lstm_model.h5')
+lemmatizer = WordNetLemmatizer()
 
 @app.route('/', methods = ['GET','POST'])
 def home():
-
     return render_template('home.html')
 
 @app.route('/sentiment_analysis', methods = ['GET','POST'])
 def sent_analysis():
     text = request.form['text']
     text = re.sub('[^a-zA-Z]', ' ', text).lower()
-    words = text.split()
+    text = text.split()
 
-    word_to_id = imdb.get_word_index()
-    words = [[word_to_id[word] if (word in word_to_id and word_to_id[word] <=2000) else 0 for word in words]]
-    x_test = sequence.pad_sequences(words, maxlen = 2000)
+    # old method
+    #words = text.split()
+    #word_to_id = imdb.get_word_index()
+   # words = [[word_to_id[word] if (word in word_to_id and word_to_id[word] <=2000) else 0 for word in words]]
+    words = [lemmatizer.lemmatize(word) for word in text if word not in stopwords.words('english')]
+    words = ' '.join(words)
+    one_hot_text = [one_hot(words, 5000)]
+    test_text = sequence.pad_sequences(one_hot_text, maxlen = 40)
 
-    predict_prob = model.predict(x_test)
+    predict_prob = model.predict(test_text)
     sentiment = ''
     if predict_prob > 0.5:
         sentiment = "Postive"
-        img_file = os.path.join(IMAGE_FOLDER, 'Smilling_Emoji.png')
+        img_file = os.path.join(IMAGE_FOLDER, 'Smiling_Emoji.png')
         
     else:
         sentiment = "Negetive"
         img_file = os.path.join(IMAGE_FOLDER, 'Sad_Emoji.png')
 
     percentage = (predict_prob  - 0.5)*100
+    percentage = percentage[0]
 
     return render_template('home.html', text = text, sentiment = sentiment, sent_percent = percentage, image = img_file)
 
